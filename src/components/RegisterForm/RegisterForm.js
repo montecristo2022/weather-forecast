516aae76429391a501d074d3447be9dbc92eadfa
montecristo2022 <104501641+montecristo2022@users.noreply.github.com>
2023-05-08 16:11:02 +0300

    fix

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,94 +1,3 @@
-// import { useDispatch } from "react-redux";
-// import { register } from "../../redux/auth/operations";
-// import { TextField, Button, Box, Typography } from "@mui/material";
-// import { styled } from "@mui/system";
-// import { useNavigate } from "react-router-dom";
-
-// export const RegisterForm = () => {
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     const form = e.currentTarget;
-//     dispatch(
-//       register({
-//         name: form.elements.name.value,
-//         email: form.elements.email.value,
-//         password: form.elements.password.value,
-//       })
-//     ).then(() => {
-//       navigate("/");
-//     });
-//     form.reset();
-//   };
-
-//   const StyledForm = styled("form")(({ theme }) => ({
-//     display: "flex",
-//     flexDirection: "column",
-//     gap: "1rem",
-//     maxWidth: "700px",
-//     width: "100%",
-//     margin: "auto",
-//     [theme.breakpoints.down("sm")]: {
-//       width: "90%",
-//     },
-//   }));
-
-//   const StyledContainer = styled("div")({
-//     display: "flex",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     height: "100vh",
-//     width: "100vw",
-//   });
-
-
-// const GoBackButton = styled("button")({
-//   width: "100px",
-//   backgroundColor: "#1976d2",
-//   color: "white",
-//   borderRadius: "4px",
-//   border: "none",
-//   padding: "8px 16px",
-//   cursor: "pointer",
-//   fontSize: "16px",
-//   fontWeight: "bold",
-//   "&:hover, &:focus": {
-//     backgroundColor: "#115293",
-//   },
-// });
-
-
-
-//   return (
-//     <StyledContainer>
-//       <StyledForm onSubmit={handleSubmit} autoComplete="off">
-//         <Typography variant="h6">Register</Typography>
-//         <TextField label="Username" name="name" variant="outlined" />
-//         <TextField label="Email" name="email" type="email" variant="outlined" />
-//         <TextField
-//           label="Password"
-//           name="password"
-//           type="password"
-//           variant="outlined"
-//         />
-//         <Button type="submit" variant="contained" color="primary">
-//           Register
-//         </Button>
-
-//         {/* <GoBackButton>Go Back</GoBackButton> */}
-//       </StyledForm>
-//     </StyledContainer>
-//   );
-// };
-
-
-
-
-
-
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { register } from "../../redux/auth/operations";
 import { TextField, Button, Box, Typography } from "@mui/material";
@@ -98,60 +7,15 @@ import { useNavigate } from "react-router-dom";
 export const RegisterForm = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [errorMessage, setErrorMessage] = useState({ name: "", email: "", password: "" });
-
-  const validateUsername = (username) => {
-    return username.length >= 3;
-  };
-
-  const validateEmail = (email) => {
-    const emailParts = email.split("@");
-    return emailParts.length === 2 && emailParts[0].length >= 3;
-  };
-
-  const validatePassword = (password) => {
-    return password.length >= 7;
-  };
-
-   const handleGoBack = () => {
-    navigate("/");
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.currentTarget;
-    const name = form.elements.name.value;
-    const email = form.elements.email.value;
-    const password = form.elements.password.value;
-
-    let validationError = {
-      name: "",
-      email: "",
-      password: "",
-    };
-
-    if (!validateUsername(name)) {
-      validationError.name = "Имя пользователя должно содержать минимум 3 символа";
-    }
-
-    if (!validateEmail(email)) {
-      validationError.email = "E-mail должен содержать минимум 3 символа перед @";
-    }
-
-    if (!validatePassword(password)) {
-      validationError.password = "Пароль должен содержать минимум 7 символов";
-    }
-
-    if (validationError.name || validationError.email || validationError.password) {
-      setErrorMessage(validationError);
-      return;
-    }
-
     dispatch(
       register({
-        name: name,
-        email: email,
-        password: password,
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        password: form.elements.password.value,
       })
     ).then(() => {
       navigate("/");
@@ -179,7 +43,8 @@ export const RegisterForm = () => {
     width: "100vw",
   });
 
-  const GoBackButton = styled("button")({
+
+const GoBackButton = styled("button")({
   width: "100px",
   backgroundColor: "#1976d2",
   color: "white",
@@ -194,40 +59,175 @@ export const RegisterForm = () => {
   },
 });
 
+
+
   return (
     <StyledContainer>
       <StyledForm onSubmit={handleSubmit} autoComplete="off">
         <Typography variant="h6">Register</Typography>
-        <TextField
-          label="Username"
-          name="name"
-          variant="outlined"
-          error={!!errorMessage.name}
-          helperText={errorMessage.name}
-        />
-        <TextField
-          label="Email"
-          name="email"
-          type="email"
-          variant="outlined"
-          error={!!errorMessage.email}
-          helperText={errorMessage.email}
-        />
+        <TextField label="Username" name="name" variant="outlined" />
+        <TextField label="Email" name="email" type="email" variant="outlined" />
         <TextField
           label="Password"
           name="password"
           type="password"
           variant="outlined"
-          error={!!errorMessage.password}
-          helperText={errorMessage.password}
         />
         <Button type="submit" variant="contained" color="primary">
           Register
         </Button>
 
-        <GoBackButton onClick={handleGoBack}>Go Back</GoBackButton>
+        {/* <GoBackButton>Go Back</GoBackButton> */}
       </StyledForm>
     </StyledContainer>
   );
 };
+
+
+
+
+
+
+// import { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { register } from "../../redux/auth/operations";
+// import { TextField, Button, Box, Typography } from "@mui/material";
+// import { styled } from "@mui/system";
+// import { useNavigate } from "react-router-dom";
+
+// export const RegisterForm = () => {
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const [errorMessage, setErrorMessage] = useState({ name: "", email: "", password: "" });
+
+//   const validateUsername = (username) => {
+//     return username.length >= 3;
+//   };
+
+//   const validateEmail = (email) => {
+//     const emailParts = email.split("@");
+//     return emailParts.length === 2 && emailParts[0].length >= 3;
+//   };
+
+//   const validatePassword = (password) => {
+//     return password.length >= 7;
+//   };
+
+//    const handleGoBack = () => {
+//     navigate("/");
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     const form = e.currentTarget;
+//     const name = form.elements.name.value;
+//     const email = form.elements.email.value;
+//     const password = form.elements.password.value;
+
+//     let validationError = {
+//       name: "",
+//       email: "",
+//       password: "",
+//     };
+
+//     if (!validateUsername(name)) {
+//       validationError.name = "Имя пользователя должно содержать минимум 3 символа";
+//     }
+
+//     if (!validateEmail(email)) {
+//       validationError.email = "E-mail должен содержать минимум 3 символа перед @";
+//     }
+
+//     if (!validatePassword(password)) {
+//       validationError.password = "Пароль должен содержать минимум 7 символов";
+//     }
+
+//     if (validationError.name || validationError.email || validationError.password) {
+//       setErrorMessage(validationError);
+//       return;
+//     }
+
+//     dispatch(
+//       register({
+//         name: name,
+//         email: email,
+//         password: password,
+//       })
+//     ).then(() => {
+//       navigate("/");
+//     });
+//     form.reset();
+//   };
+
+//   const StyledForm = styled("form")(({ theme }) => ({
+//     display: "flex",
+//     flexDirection: "column",
+//     gap: "1rem",
+//     maxWidth: "700px",
+//     width: "100%",
+//     margin: "auto",
+//     [theme.breakpoints.down("sm")]: {
+//       width: "90%",
+//     },
+//   }));
+
+//   const StyledContainer = styled("div")({
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     height: "100vh",
+//     width: "100vw",
+//   });
+
+//   const GoBackButton = styled("button")({
+//   width: "100px",
+//   backgroundColor: "#1976d2",
+//   color: "white",
+//   borderRadius: "4px",
+//   border: "none",
+//   padding: "8px 16px",
+//   cursor: "pointer",
+//   fontSize: "16px",
+//   fontWeight: "bold",
+//   "&:hover, &:focus": {
+//     backgroundColor: "#115293",
+//   },
+// });
+
+//   return (
+//     <StyledContainer>
+//       <StyledForm onSubmit={handleSubmit} autoComplete="off">
+//         <Typography variant="h6">Register</Typography>
+//         <TextField
+//           label="Username"
+//           name="name"
+//           variant="outlined"
+//           error={!!errorMessage.name}
+//           helperText={errorMessage.name}
+//         />
+//         <TextField
+//           label="Email"
+//           name="email"
+//           type="email"
+//           variant="outlined"
+//           error={!!errorMessage.email}
+//           helperText={errorMessage.email}
+//         />
+//         <TextField
+//           label="Password"
+//           name="password"
+//           type="password"
+//           variant="outlined"
+//           error={!!errorMessage.password}
+//           helperText={errorMessage.password}
+//         />
+//         <Button type="submit" variant="contained" color="primary">
+//           Register
+//         </Button>
+
+//         <GoBackButton onClick={handleGoBack}>Go Back</GoBackButton>
+//       </StyledForm>
+//     </StyledContainer>
+//   );
+// };
 
